36990f0afcfb610d275ba0be568ac477aaf21c65
hansol9 <hansol9@gmail.com>
2020-03-11 19:59:29 -0400

    initial commit

---
+++ src/main/java/com/hansol/restfulwebservice/RestfulWebServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hansol.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansol/restfulwebservice/RestfulWebServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.restfulwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
